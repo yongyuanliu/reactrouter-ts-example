3ffbbff665110451b8ffc222267e9dbea6c60ee3
Yongyuan Liu <stormbluer@outlook.com>
2021-12-04 22:38:17 +0800

    react router auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from './modal/app';
+import App from './auth/app';
 
 const reactRoot = document.querySelector("#react_root");
 ReactDOM.render(
