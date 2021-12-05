b8f52e80b5ba88648fb69e61e650631555d596f3
Yongyuan Liu <stormbluer@outlook.com>
2021-12-05 09:56:29 +0800

    custom link

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from './auth/app';
+import App from './custom-link/App';
 
 const reactRoot = document.querySelector("#react_root");
 ReactDOM.render(
