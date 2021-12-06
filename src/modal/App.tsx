88ddc2a1de13d1a963f078ed4e2250fc02447b2a
Yongyuan Liu <stormbluer@outlook.com>
2021-12-06 23:18:37 +0800

    import css

--- src/modal/App.tsx
+++ src/modal/App.tsx
@@ -1,62 +1,64 @@
 import React from "react";
 import { Link, Outlet, useLocation, Routes, Route, useParams, useNavigate } from "react-router-dom";
-import { IMAGES, getImageById } from "./images";
-import "./app.css";
+
+
 import Dialog from "@reach/dialog";
+import "@reach/dialog/styles.css";
+import "./app.css";
+
+import { IMAGES, getImageById } from "./images";
 export default function App() {
     let location = useLocation();
     let state = location.state as { backgroundLocation?: Location };
     return (
-        <>
-            <div>
-                <h1>Modal Example</h1>
+        <div>
+            <h1>Modal Example</h1>
 
-                <p>
-                    This is an example of how to create a contextual modal navigation with
-                    React Router where the navigation path the user takes determines if the
-                    page is rendered in the modal or not (popularized by pinterest,
-                    instagram, and others in the 2010s). This type of modal is typically
-                    used as a kind of "detail" view to focus on a particular object in a
-                    collection (like a pinterest board) while not taking you completely out
-                    of context of the parent page. But, when the same URL is visited
-                    directly (rather than from the collection page) it renders as it's own
-                    full page instead of in a modal.
-                </p>
+            <p>
+                This is an example of how to create a contextual modal navigation with
+                React Router where the navigation path the user takes determines if the
+                page is rendered in the modal or not (popularized by pinterest,
+                instagram, and others in the 2010s). This type of modal is typically
+                used as a kind of "detail" view to focus on a particular object in a
+                collection (like a pinterest board) while not taking you completely out
+                of context of the parent page. But, when the same URL is visited
+                directly (rather than from the collection page) it renders as it's own
+                full page instead of in a modal.
+            </p>
 
-                <p>
-                    In this example, notice how the URL updates when the modal opens (if you
-                    are viewing the example in StackBlitz you may need to open in a new
-                    browser window). Even though the URL is updated to the specific item in
-                    the modal, the background page is still showing behind it.
-                </p>
+            <p>
+                In this example, notice how the URL updates when the modal opens (if you
+                are viewing the example in StackBlitz you may need to open in a new
+                browser window). Even though the URL is updated to the specific item in
+                the modal, the background page is still showing behind it.
+            </p>
 
-                <p>
-                    Next, copy and paste the URL to a new browser tab and notice that it
-                    shows that specific item not in a modal, but directly on the page. This
-                    is the view that someone would see if they clicked on a link that you
-                    sent them when you had the modal open. They don't have the context you
-                    did when you opened the modal, so they don't see it in the context of
-                    the background page.
-                </p>
+            <p>
+                Next, copy and paste the URL to a new browser tab and notice that it
+                shows that specific item not in a modal, but directly on the page. This
+                is the view that someone would see if they clicked on a link that you
+                sent them when you had the modal open. They don't have the context you
+                did when you opened the modal, so they don't see it in the context of
+                the background page.
+            </p>
 
-                <Routes location={state?.backgroundLocation || location}>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />}></Route>
-                        <Route path="gallery" element={<Gallery />}></Route>
-                        <Route path="/img/:id" element={<ImageView />}></Route>
-                        <Route path="*" element={<NoMatch />}></Route>
-                    </Route>
-                </Routes>
+            <Routes location={state?.backgroundLocation || location}>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />}></Route>
+                    <Route path="gallery" element={<Gallery />}></Route>
+                    <Route path="/img/:id" element={<ImageView />}></Route>
+                    <Route path="*" element={<NoMatch />}></Route>
+                </Route>
+            </Routes>
 
-                {
-                    state?.backgroundLocation && (
-                        <Routes>
-                            <Route path="/img/:id" element={<Modal />}></Route>
-                        </Routes>
-                    )
-                }
-            </div>
-        </>
+            {
+                state?.backgroundLocation && (
+                    <Routes>
+                        <Route path="/img/:id" element={<Modal />}></Route>
+                    </Routes>
+                )
+            }
+        </div>
     );
 }
 
@@ -146,7 +148,6 @@ function Modal() {
 
     return (
         <Dialog
-            isOpen
             aria-labelledby="label"
             onDismiss={onDismiss}
             initialFocusRef={buttonRef}>
